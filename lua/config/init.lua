ba46b80868d8a65b79c801cfd24c057c9552be34
Murad Imanov <imanovmurad896@gmail.com>
2025-10-14 11:53:36 +0300

    feat: improve config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,3 +6,16 @@ require("config.lazy")
 
 vim.lsp.enable("ts_ls")
 
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		vim.keymap.set('n', '<leader>ai', function()
+			vim.lsp.buf.code_action({
+				context = {
+					only = { "source.addMissingImports.ts" },
+					diagnostics = {},
+				},
+			})
+		end, { desc = "[a]dd missing [i]mports (TS)" })
+	end,
+})
--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -4,3 +4,7 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>re", vim.cmd.Rex)
 
+vim.keymap.set('n', '<Esc>', function()
+	vim.cmd('nohlsearch')
+	return '<Esc>'
+end, { expr = true, silent = true })
